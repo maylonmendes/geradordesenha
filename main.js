f2775236da4f8263baf014a867ddbeaf10ce37f9
maylonmendes <141340388+maylonmendes@users.noreply.github.com>
2025-06-06 07:50:48 -0300

    .

--- main.js
+++ main.js
@@ -21,6 +21,16 @@ function aumentaTamanho() {
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+
+for (i = 0; i < checkbox.length; i++) {
+    checkbox[i].onclick = geraSenha;
+}
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
 geraSenha();
 
 function geraSenha() {
